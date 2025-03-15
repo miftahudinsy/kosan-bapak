4c3747642c13731881210a3a4fdd0eb6df82b802
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-03-15 15:00:28 +0700

    ubah bahasa halaman menjadi Indonesia dan tambahkan pengaturan dinamis untuk layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,17 +20,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Analytics />
-      </body>
+    <html lang="id">
+      <body>{children}</body>
     </html>
   );
 }
+
+export const dynamic = "force-dynamic";
