462ab81209b031db8211c6b07a00e0ef8ffdeb4a
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-03-18 12:41:51 +0700

    tambah komponen SpeedInsights dari Vercel ke dalam layout untuk analisis performa dan perbarui package.json serta package-lock.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
